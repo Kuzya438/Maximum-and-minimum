13852aba1e16939de3bd292dbeeebccb20ef91b0
Ivan <ivankuznetsov454@yandex.ru>
2026-03-25 22:09:26 +0500

    Maximum and minimum

---
+++ Maximum and minimum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	const int size{ 10 };
+	int integerArray[size]{ 55, 211, 3, 43, 5, 76, 7, 8, 947, 10 };
+	int min{ integerArray[0] }, max{};
+
+	std::cout << "Массив: ";
+
+	for (int element{}; element < size; ++element) {
+		std::cout << integerArray[element] << " ";
+		if (min >= integerArray[element]) { min = integerArray[element]; }
+		if (max <= integerArray[element]) { max = integerArray[element]; }
+	}
+
+	std::cout << std::endl << "Минимальный элемент: " << min << std::endl;;
+	std::cout << "Максимальный элемент: " << max << std::endl;;
+
+	return EXIT_SUCCESS;
+}
